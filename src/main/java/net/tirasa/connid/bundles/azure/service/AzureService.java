6e3586f89aa54e4ec9947995172f9354465aacce
Francesco Chicchiriccò <ilgrosso@apache.org>
2018-03-16 10:29:58 +0100

    Reducing overall size (take 2)

--- src/main/java/net/tirasa/connid/bundles/azure/service/AzureService.java
+++ src/main/java/net/tirasa/connid/bundles/azure/service/AzureService.java
@@ -24,7 +24,6 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
